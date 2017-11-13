d529cabe6b391758221b96c8c5de64f0a9e66efb
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-13 15:19:56 +0100

    Commit 3. Code changed to detect if even number is <= 5 or > 5.

--- src/OddEven.java
+++ src/OddEven.java
@@ -8,8 +8,11 @@ public class OddEven {
 		System.out.println(tal);
 		System.out.println("\nKoden är slut.");
 		
-		if (tal%2==0) {			//"Om resten efter division med 2 är 0"
-			System.out.println("Talet är jämnt.");
+		if (tal%2==0 && tal <= 5) {			//"Om resten efter division med 2 är 0"
+			System.out.println("Talet är jämnt och det är <= 5.");
+		}
+		else if (tal%2==0 && tal > 5){
+			System.out.println("Talet är jämnt och större än 5");
 		}
 		else {
 			System.out.println("Talet är udda.");
