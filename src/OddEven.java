55fcd780f680a3ba138c75cfe04e652343e8926d
Anna-Maja Lithner <am_lithner@hotmail.com>
2017-11-13 15:13:08 +0100

    Commit 1. Add total project including OddEven.java.

---
+++ src/OddEven.java
@@ -0,0 +1,18 @@
+/*skapa ett program som mha remainder avgör om ett tal rä udda eller jämnt.*/
+import java.util.Random; 
+public class OddEven {
+	public static void main(String[]args) {
+		
+		Random random = new Random();
+		int tal = random.nextInt(10);
+		System.out.println(tal);
+		
+		if (tal%2==0) {			//"Om resten efter division med 2 är 0"
+			System.out.println("Talet är jämnt.");
+		}
+		else {
+			System.out.println("Talet är udda.");
+		}
+	}
+
+}
